3494d0e498a24264c8ae7b4a0867b35c74c8ea1a
Sayantha Hansaka <sayanthahansaka00@gmail.com>
2023-12-29 14:37:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='bg-red font-primary'>Yowwwwwwwwwwww</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "red" : "#FF4500", 
+        "green" : "#3CB371",
+        "secondary" : "#555",
+        "primaryBG" : "#FCFCFC"
+      },
+      fontFamily: {
+        "primary" : ['Inter','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
